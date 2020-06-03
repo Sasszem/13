e03ab7460a368f36d0b71b053eda7a46cea35865
Sasszem <barath.laci10@gmail.com>
2020-06-03 03:06:41 +0200

    Add Path base

--- main.lua
+++ main.lua
@@ -24,3 +24,24 @@ function love.keypressed(key, code, rep)
         love.event.quit()
     end
 end
+
+local function mouseToWorld(x, y)
+    if config.flip then
+        return 2*x, 2*y
+    end
+    return x, y
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    playfield:touchBegin(mouseToWorld(x, y))
+end
+
+function love.mousemoved(x, y, dx, dy, istouch)
+    if love.mouse.isDown(1, 2, 3) then
+        playfield:touchMove(mouseToWorld(x, y))
+    end
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+    playfield:touchEnd()
+end
