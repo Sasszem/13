3c6d03c2cf5a5686b6ab6b4b23824a4c9879c1c6
Sasszem <barath.laci10@gmail.com>
2020-06-13 01:06:10 +0200

    Menu sound

--- src/menu/buttonStyle.lua
+++ src/menu/buttonStyle.lua
@@ -1,3 +1,4 @@
+local Sounds = require("src.Sounds")
 local ButtonStyle = {
     activeBorder = rgb(41, 163, 41),
     borderColor = rgb(179, 0, 0),
@@ -6,6 +7,7 @@ local ButtonStyle = {
 
 function ButtonStyle:mouseEnter(x, y)
     self.style.borderColor, self.style.activeBorder = self.style.activeBorder, self.style.borderColor
+    Sounds.play("click")
 end
 
 function ButtonStyle:mouseLeave(x, y)
