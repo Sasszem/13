3fc30161bcb7776e13fd5a8d4ac48f6b89a71749
Sasszem <barath.laci10@gmail.com>
2020-06-14 01:29:02 +0200

    Refactor highscores api -  add comments - change load to accept gamemode instead of filename

--- src/Highscores.lua
+++ src/Highscores.lua
@@ -6,9 +6,16 @@ local KEEP = 5
 
 -- format: result-month/day hour:minute
 
-function Highscores.load(fn)
+--- load highscores
+-- @param gamemode the gamemode to load highscores for
+-- @return a list of result(num) - date (str) pairs
+function Highscores.load(gamemode)
+    -- save file name
+    local fn = FILENAME:format(gamemode)
+
     local data = {}
     if love.filesystem.getInfo(fn) then
+        -- parse each line and append to data
         for line in love.filesystem.lines(fn) do
             local entry = {}
             entry[1], entry[2] = line:match("(.+)-(.+)")
@@ -16,9 +23,13 @@ function Highscores.load(fn)
             data[#data+1] = entry
         end
     end
+
     return data
 end
 
+--- save highscore data to file. Used itnernally.
+-- @param fn - the file name to save into
+-- @param data a list of result-date pairs
 function Highscores.save(fn, data)
     local f = love.filesystem.newFile(fn, "w")
     for _, d in ipairs(data) do
@@ -27,24 +38,44 @@ function Highscores.save(fn, data)
     f:close()
 end
 
+--- update highsocres
+-- @param gamemode the current gamemode
+-- @time the time of the game
+-- @maximum the biggest cell value in game
 function Highscores.update(gamemode, time, maximum)
     local fn = FILENAME:format(gamemode)
-    local d = Highscores.load(fn)
+    
+    -- load old highscores
+    local scores = Highscores.load(gamemode)
+
+    -- get current date string
     local dT = os.date("*t")
     local dateString = ("%02d/%02d %2d:%02d"):format(dT.month, dT.day, dT.hour, dT.min)
+
+    -- result from values and gamemode
     local res = gamemode=="timed" and maximum or time
-    d[#d+1] = {res, dateString}
+
+    -- construct highscores entry
+    scores[#scores+1] = {res, dateString}
+
+    -- do we need to sorty ascending or descending
     local reverse = (gamemode == "timed")
-    table.sort(d, function(a, b)
+
+    -- sort - custom sort function only adds reverse
+    table.sort(scores, function(a, b)
         if reverse then
             a, b = b, a
         end
         return a[1] < b[1]
     end)
-    while  #d > KEEP do
-        d[#d] = nil
+
+    -- remove last entries if we have more than the target number
+    while  #scores > KEEP do
+        scores[#scores] = nil
     end
-    Highscores.save(fn, d)
+
+    -- save the highscores
+    Highscores.save(fn, scores)
 end
 
 return Highscores
