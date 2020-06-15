4461e237cffd988b863895fa37a0a670f7e6dcaa
Sasszem <barath.laci10@gmail.com>
2020-06-15 03:18:13 +0200

    New release script: love-release

--- conf.lua
+++ conf.lua
@@ -1,5 +1,19 @@
 function love.conf(t)
     t.window.width = 360 --1080
     t.window.height = 679--1920
-    --t.window.fullscreen = true
+
+    t.releases = {
+        title = "13",
+        package = "13",
+        loveVersion = "11.3",
+        version = "1.0",
+        author = "Sasszem",
+        email = "barath.laszlo.szolnok@gmail.com",
+        description = "A simple but addicitve game",
+        homepage = "https://github.com/sasszem/13",
+        excludeFileList = {
+            ".git",
+            ".vscode"
+        },
+      }
 end
