05236e17529063fdfde506bf6f6259dacdce1334
Btimes2 <64044481+Btimes2@users.noreply.github.com>
2020-08-19 18:56:23 +0200

    Changed the color of tile 10 to a darker red, and the color of tile 14 and 15 to flat white (line tile 1), because they will never be used anyway. (#64)
    Also changed some tabs to spaces, but that was not intended and does not do any harm.
    
    closes #61

--- src/Cell.lua
+++ src/Cell.lua
@@ -34,12 +34,12 @@ local CellColors = {
     hex("#3465a4"),
     hex("#f57900"),
     hex("#babdb6"),
-    hex("#cc0000"), -- 10
+    hex("#a40000"), -- 10 
     hex("#c4a000"),
     hex("#204a87"),
     hex("#4e9a06"),
-    hex("#5c3566"),
-    hex("#a40000"), -- 15
+    hex("#eeeeec"),
+    hex("#eeeeec"), -- 15
 }
 
 --------------------
@@ -120,21 +120,21 @@ function rwrc(x, y, w, h, r)
     -- without border lines, cell lines will not be blurred
     -- but, with border lines, cell will be ugly when drawing a path
     love.graphics.rectangle("line", x, y+r, w, h-r*2)
-	love.graphics.rectangle("line", x+r, y, w-r*2, r)
-	love.graphics.rectangle("line", x+r, y+h-r, w-r*2, r)
-	love.graphics.arc("line", x+r, y+r, r, left, top)
-	love.graphics.arc("line", x + w-r, y+r, r, -bottom, right)
-	love.graphics.arc("line", x + w-r, y + h-r, r, right, bottom)
-	love.graphics.arc("line", x+r, y + h-r, r, bottom, left)
+    love.graphics.rectangle("line", x+r, y, w-r*2, r)
+    love.graphics.rectangle("line", x+r, y+h-r, w-r*2, r)
+    love.graphics.arc("line", x+r, y+r, r, left, top)
+    love.graphics.arc("line", x + w-r, y+r, r, -bottom, right)
+    love.graphics.arc("line", x + w-r, y + h-r, r, right, bottom)
+    love.graphics.arc("line", x+r, y + h-r, r, bottom, left)
     
     -- filling
     love.graphics.rectangle("fill", x, y+r, w, h-r*2)
-	love.graphics.rectangle("fill", x+r, y, w-r*2, r)
-	love.graphics.rectangle("fill", x+r, y+h-r, w-r*2, r)
-	love.graphics.arc("fill", x+r, y+r, r, left, top)
-	love.graphics.arc("fill", x + w-r, y+r, r, -bottom, right)
-	love.graphics.arc("fill", x + w-r, y + h-r, r, right, bottom)
-	love.graphics.arc("fill", x+r, y + h-r, r, bottom, left)
+    love.graphics.rectangle("fill", x+r, y, w-r*2, r)
+    love.graphics.rectangle("fill", x+r, y+h-r, w-r*2, r)
+    love.graphics.arc("fill", x+r, y+r, r, left, top)
+    love.graphics.arc("fill", x + w-r, y+r, r, -bottom, right)
+    love.graphics.arc("fill", x + w-r, y + h-r, r, right, bottom)
+    love.graphics.arc("fill", x+r, y + h-r, r, bottom, left)
 end
 
 
