4789aac924fb2bae0a6a41019a446fcc56d87b25
Sasszem <barath.laszlo.szolnok@gmail.com>
2021-09-29 18:17:11 +0200

    Fix: undo problems

--- src/Undo.lua
+++ src/Undo.lua
@@ -40,17 +40,12 @@ function Undo:restore()
     -- do not restore if no data
     -- (no backup was made yet)
     if self.biggestYet == 0 then return end
-
+    if self.game.animating then return end
     self.game.TM:run(self.undoTask, self)
 end
 
 
 function Undo:undoTask()
-    -- wait for game to finish animating
-    while self.game.animating do
-        local _ = coroutine.yield()
-    end
-
     -- deep copy the saved data back to game
     self.game.time = self.time
 
